0d64d973ab331aad74b53c83f10b0fccf3a810f9
AbdelrahmanAhmed240 <abdelrahmanaly065@gmail.com>
2025-04-28 00:34:17 +0300

    Add files via upload

---
+++ Functions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Quiz_Managment_System
+{
+    class Functions
+    {
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection("Data Source = BODA004; Initial Catalog = \"Exam System\"; Integrated Security = True; Encrypt=True;TrustServerCertificate=True");
+        }
+
+        public DataSet getData(string query)
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                return ds;
+            }
+        }
+
+        public void SetData(string query, string msg)
+        {
+            try
+            {
+                using (SqlConnection conn = GetConnection())
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    MessageBox.Show(msg + " Rows Affected: " + rowsAffected, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error executing query: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public SqlDataReader getForCombo(string query)
+        {
+            SqlConnection conn = GetConnection();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataReader sdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            return sdr;
+        }
+    }
+}
